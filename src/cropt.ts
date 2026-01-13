devtheorem/cropt

commit 00dd1250833a15bbaa2c9ece8e659a6a60a08cdf

Fix transform state when zooming while dragging

src/cropt.ts
@@ -408,26 +408,26 @@ export class Cropt {
         };
     }
 
+    #assignTransformCoordinates(deltaX: number, deltaY: number) {
+        const imgRect = this.elements.preview.getBoundingClientRect();
+        const vpRect = this.elements.viewport.getBoundingClientRect();
+        const transform = Transform.parse(this.elements.preview);
+
+        if (vpRect.top > imgRect.top + deltaY && vpRect.bottom < imgRect.bottom + deltaY) {
+            transform.y = transform.y + deltaY;
+        }
+
+        if (vpRect.left > imgRect.left + deltaX && vpRect.right < imgRect.right + deltaX) {
+            transform.x = transform.x + deltaX;
+        }
+
+        this.#updateCenterPoint(transform);
+        this.#updateOverlayDebounced();
+    }
+
     #initDraggable() {
-        var originalX = 0;
-        var originalY = 0;
-        var transform: Transform;
-        var vpRect: DOMRect;
-
-        let assignTransformCoordinates = (deltaX: number, deltaY: number) => {
-            var imgRect = this.elements.preview.getBoundingClientRect(),
-                top = transform.y + deltaY,
-                left = transform.x + deltaX;
-
-            if (vpRect.top > imgRect.top + deltaY && vpRect.bottom < imgRect.bottom + deltaY) {
-                transform.y = top;
-            }
-
-            if (vpRect.left > imgRect.left + deltaX && vpRect.right < imgRect.right + deltaX) {
-                transform.x = left;
-            }
-        };
-
+        let originalX = 0;
+        let originalY = 0;
         let pEventCache: PointerEvent[] = [];
         let origPinchDistance = 0;
 
@@ -453,12 +453,7 @@ export class Cropt {
                 return; // ignore single pointer movement after pinch zoom
             }
 
-            let deltaX = ev.pageX - originalX;
-            let deltaY = ev.pageY - originalY;
-            assignTransformCoordinates(deltaX, deltaY);
-            this.elements.preview.style.transform = transform.toString();
-
-            this.#updateOverlayDebounced();
+            this.#assignTransformCoordinates(ev.pageX - originalX, ev.pageY - originalY);
             originalX = ev.pageX;
             originalY = ev.pageY;
         };
@@ -474,7 +469,6 @@ export class Cropt {
                 this.elements.overlay.removeEventListener('pointercancel', pointerUp);
 
                 this.#setDragState(false, this.elements.preview);
-                this.#updateCenterPoint();
                 origPinchDistance = 0;
             }
         };
@@ -495,8 +489,6 @@ export class Cropt {
             originalX = ev.pageX;
             originalY = ev.pageY;
             this.#setDragState(true, this.elements.preview);
-            transform = Transform.parse(this.elements.preview);
-            vpRect = this.elements.viewport.getBoundingClientRect();
 
             this.elements.overlay.addEventListener('pointermove', pointerMove);
             this.elements.overlay.addEventListener('pointerup', pointerUp);
@@ -516,14 +508,7 @@ export class Cropt {
             } else if (["ArrowUp", "ArrowDown", "ArrowLeft", "ArrowRight"].includes(ev.key)) {
                 ev.preventDefault();
                 let [deltaX, deltaY] = getArrowKeyDeltas(ev.key);
-
-                transform = Transform.parse(this.elements.preview);
-                vpRect = this.elements.viewport.getBoundingClientRect();
-                assignTransformCoordinates(deltaX, deltaY);
-                this.elements.preview.style.transform = transform.toString();
-
-                this.#updateOverlayDebounced();
-                this.#updateCenterPoint();
+                this.#assignTransformCoordinates(deltaX, deltaY);
             }
         };
 
@@ -646,11 +631,10 @@ export class Cropt {
         preview.style.transformOrigin = new TransformOrigin().toString();
 
         this.#centerImage();
-        this.#updateCenterPoint();
         this.#updateOverlay();
     }
 
-    #updateCenterPoint() {
+    #updateCenterPoint(transform: Transform) {
         var scale = this.#scale,
             data = this.elements.preview.getBoundingClientRect(),
             vpData = this.elements.viewport.getBoundingClientRect(),
@@ -664,7 +648,6 @@ export class Cropt {
             y: (center.y - pc.y) * (1 - scale),
         };
 
-        const transform = Transform.parse(this.elements.preview);
         transform.x -= adj.x;
         transform.y -= adj.y;
 
@@ -702,6 +685,6 @@ export class Cropt {
             h = vpTop - ((imgDim.height - vpDim.height) / 2),
             transform = new Transform(w, h, this.#scale);
 
-        this.elements.preview.style.transform = transform.toString();
+        this.#updateCenterPoint(transform);
     }
 }

commit dbe5ef75aa29ae887c0434c10dc65cebd085786e

Fix incorrect min zoom and center point calculation
Center point calculation was broken by 00dd1250833a15bbaa2c9ece8e659a6a60a08cdf.

Also increased step precision of zoom slider to match Croppie (previously changed in 7bcc4f57272ef8dfdd8ede1018ce71398eca6c89).

Closes #6

src/cropt.ts
@@ -56,7 +56,7 @@ function setZoomerVal(value: number, zoomer: HTMLInputElement) {
     const zMin = parseFloat(zoomer.min);
     const zMax = parseFloat(zoomer.max);
 
-    zoomer.value = Math.max(zMin, Math.min(zMax, value)).toFixed(3);
+    zoomer.value = Math.max(zMin, Math.min(zMax, value)).toFixed(4);
 }
 
 function loadImage(src: string): Promise<HTMLImageElement> {
@@ -177,7 +177,7 @@ export class Cropt {
         this.elements.boundary.appendChild(this.elements.overlay);
 
         this.elements.zoomer.type = "range";
-        this.elements.zoomer.step = "0.001";
+        this.elements.zoomer.step = "0.0001";
         this.elements.zoomer.value = "1";
         this.elements.zoomer.setAttribute("aria-label", "zoom");
 
@@ -644,6 +644,7 @@ export class Cropt {
     }
 
     #updateCenterPoint(transform: Transform) {
+        this.elements.preview.style.transform = transform.toString();
         const vpData = this.elements.viewport.getBoundingClientRect();
         const data = this.elements.preview.getBoundingClientRect();
         const curPos = new TransformOrigin(this.elements.preview);
@@ -675,8 +676,9 @@ export class Cropt {
             maxZoom += minZoom;
         }
 
-        this.elements.zoomer.min = minZoom.toFixed(3);
-        this.elements.zoomer.max = maxZoom.toFixed(3);
+        // min zoom cannot be rounded, or large images won't match the viewport size when zoomed out
+        this.elements.zoomer.min = minZoom.toString();
+        this.elements.zoomer.max = maxZoom.toString();
         let zoom = this.#boundZoom;
 
         if (zoom === null) {